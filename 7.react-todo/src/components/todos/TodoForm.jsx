374ce1c4686f16c59c946e38e76fa584182563b2
zzzdks760 <zzzdks760@naver.com>
2023-12-12 17:15:01 +0900

    colse 기능 추가

--- 7.react-todo/src/components/todos/TodoForm.jsx
+++ 7.react-todo/src/components/todos/TodoForm.jsx
@@ -49,7 +49,7 @@ const TodoForm = ({ onAdd, onClose }) => {
                 {isFormInvalid && <div className='mt-2 text-red-500'>모든 항목을 채워서 작성해주세요</div>}
                 
                 <div className='flex justify-end gap-4'>
-                    <button className='text-xl text-white' type='button'>Cancel</button>
+                    <button className='text-xl text-white' type='button' onClick={onClose}>Cancel</button>
                     <button className='px-6 py-3 text-xl text-red-200' type='button' onClick={addTodoHandler} disabled={isFormInvalid}>Add</button>
                 </div>
             </form>
