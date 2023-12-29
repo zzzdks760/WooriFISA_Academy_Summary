2de0ceed0711025cf653b218733492fccad61925
zzzdks760 <zzzdks760@naver.com>
2023-12-29 10:27:35 +0900

    대중교통 예제 주석 수정

--- 10.java/3.basic-syntax/step02basicsyntax/src/dev/syntax/oop/step04Commute/Main.java
+++ 10.java/3.basic-syntax/step02basicsyntax/src/dev/syntax/oop/step04Commute/Main.java
@@ -8,15 +8,15 @@ public class Main {
 		Student student2 = new Student("영철", 5000);
 		Student student3 = new Student("영숙", 2000);
  		//대중교통 생성
-		Transport busTransport = new Transport();
-		Transport subwayTransport = new Transport();
-		Bus bus = new Bus("730", busTransport);
-		Subway subway = new Subway("2호선", subwayTransport);
+		Transport bus730Transport = new Transport();
+		Transport subway2Transport = new Transport();
+		Bus bus = new Bus("730", bus730Transport);
+		Subway subway = new Subway("2호선", subway2Transport);
 		//대중교통 탑승
 		bus.boardBus(student);
 		bus.boardBus(student2);
 		subway.boardSubway(student3);
-		//학생정보 조회, 대중교통 조회
+		//학생정보 조회
 		student.studentInfo();
 		student2.studentInfo();
 		student3.studentInfo();
